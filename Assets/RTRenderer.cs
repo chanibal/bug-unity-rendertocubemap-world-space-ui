53724d6f0f91584d54e65181c15a394e61a37654
Krzysztof Bociurko <git@chanibal.pl>
2018-04-12 13:45:28 +0200

    Demo project for the bug

---
+++ Assets/RTRenderer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RTRenderer : MonoBehaviour {
+
+	public RenderTexture rtCubemap;
+	public RenderTexture rtNormal;
+
+
+	void Update () {
+		var cam = GetComponent<Camera>();
+
+		cam.RenderToCubemap(rtCubemap);
+
+		var oldTarget = cam.targetTexture;
+		cam.targetTexture = rtNormal;
+		cam.Render();
+		cam.targetTexture = oldTarget;
+	}
+
+}
